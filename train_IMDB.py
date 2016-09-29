dff0e654832e03df08ee1f28bb485daa7a186983
fyabc <fyabc@mail.ustc.edu.cn>
2016-09-29 03:15:39 -0700

    Add stochastic training on IMDB.

--- train_IMDB.py
+++ train_IMDB.py
@@ -341,6 +341,130 @@ def train_policy_IMDB():
             policy.discount_learning_rate()
 
 
+def train_deterministic_stochastic_IMDB():
+    np.random.seed(IMDBConfig['seed'])
+
+    # Loading data
+    train_x, train_y, valid_x, valid_y, test_x, test_y, \
+        train_size, valid_size, test_size = pre_process_data()
+
+    # Building model
+    imdb = IMDBModel(IMDBConfig['reload_model'])
+
+    # Loading configure settings
+    kf_valid, kf_test, \
+        valid_freq, save_freq, display_freq, \
+        save_to, patience = pre_process_config(imdb, train_size, valid_size, test_size)
+
+    # Build policy
+    input_size = imdb.get_policy_input_size()
+    print('Input size of policy network:', input_size)
+    policy = PolicyNetwork(input_size=input_size, start_b=0.)
+    policy.load_policy()
+    message('$    w = {}\n'
+            '$    b = {}'
+            .format(policy.W.get_value(), policy.b.get_value()))
+
+    # Training
+    history_errs = []
+    best_parameters = {}
+    bad_counter = 0
+
+    update_index = 0  # the number of update done
+    early_stop = False  # early stop
+    start_time = time.time()
+
+    epoch = 0
+    history_train_costs = []
+
+    try:
+        for epoch in range(IMDBConfig['epoch_per_episode']):
+            print('[Epoch {}]'.format(epoch))
+            message('[Epoch {}]'.format(epoch))
+
+            n_samples = 0
+
+            # Get new shuffled index for the training set.
+            kf = get_minibatches_idx(train_size, imdb.train_batch_size, shuffle=True)
+
+            for _, train_index in kf:
+                update_index += 1
+                imdb.use_noise.set_value(floatX(1.))
+
+                # Select the random examples for this minibatch
+                x = [train_x[t] for t in train_index]
+                y = [train_y[t] for t in train_index]
+
+                # Get the data in numpy.ndarray format
+                # This swap the axis!
+                # Return something of shape (minibatch maxlen, n samples)
+                x, mask, y = prepare_data(x, np.asarray(y, dtype='int64'))
+
+                # Policy take action here
+                probability = imdb.get_policy_input(x, mask, y, epoch)
+                if Config['train_type'] == 'stochastic':
+                    actions = policy.take_action(probability, False)
+
+                    # get masked inputs and targets
+                    x = x[:, actions]
+                    mask = mask[:, actions]
+                    y = y[actions]
+
+                    cost = imdb.f_grad_shared(x, mask, y)
+                    imdb.f_update(imdb.learning_rate)
+                elif Config['train_type'] == 'deterministic':
+                    raise NotImplementedError('Deterministic is not implemented')
+
+                history_train_costs.append(cost)
+
+                if np.isnan(cost) or np.isinf(cost):
+                    print('bad cost detected: ', cost)
+                    return 1., 1., 1.
+
+                if update_index % display_freq == 0:
+                    print('Epoch ', epoch, 'Update ', update_index, 'Cost ', cost)
+
+                if save_to and update_index % save_freq == 0:
+                    save_parameters(imdb, best_parameters, save_to, history_errs)
+
+                if update_index % valid_freq == 0:
+                    imdb.use_noise.set_value(0.)
+                    train_err = imdb.predict_error(train_x, train_y, kf)
+                    valid_err = imdb.predict_error(valid_x, valid_y, kf_valid)
+                    test_err = imdb.predict_error(test_x, test_y, kf_test)
+
+                    history_errs.append([valid_err, test_err])
+
+                    if not best_parameters or valid_err <= np.array(history_errs)[:, 0].min():
+                        best_parameters = imdb.get_parameter_values()
+                        bad_counter = 0
+
+                    print(('Train ', train_err, 'Valid ', valid_err, 'Test ', test_err))
+
+                    if len(history_errs) > patience and valid_err >= np.array(history_errs)[:-patience, 0].min():
+                        bad_counter += 1
+                        if bad_counter > patience:
+                            print('Early Stop!')
+                            early_stop = True
+                            break
+
+            print('Seen %d samples' % n_samples)
+
+            if early_stop:
+                break
+    except KeyboardInterrupt:
+        print('Training interrupted')
+
+    end_time = time.time()
+
+    test_and_post_process(imdb,
+                          train_size, train_x, train_y, valid_x, valid_y, test_x, test_y,
+                          kf_valid, kf_test,
+                          history_errs, best_parameters,
+                          epoch, start_time, end_time,
+                          False)
+
+
 if __name__ == '__main__':
     process_before_train(IMDBConfig)
 
@@ -348,5 +472,9 @@ if __name__ == '__main__':
         train_raw_IMDB()
     elif Config['train_type'] == 'policy':
         train_policy_IMDB()
+    elif Config['train_type'] == 'deterministic':
+        train_deterministic_stochastic_IMDB()
+    elif Config['train_type'] == 'stochastic':
+        train_deterministic_stochastic_IMDB()
     else:
         raise Exception('Unknown train type {}'.format(Config['train_type']))
