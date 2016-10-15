ea86822171d7db20fa06261ac5cf3942eb34c266
fyabc <fyabc@mail.ustc.edu.cn>
2016-10-15 04:50:45 -0700

    Fix in speed reward.

--- train_IMDB.py
+++ train_IMDB.py
@@ -363,8 +363,14 @@ def train_policy_IMDB():
         # Updating policy
         if PolicyConfig['speed_reward']:
             if first_over_index is None:
-                first_over_index = epoch + 1
-            policy.update(np.log(IMDBConfig['epoch_per_episode']) - np.log(first_over_index + 1))
+                first_over_index = update_index + 1
+            terminal_reward = float(first_over_index) / (len(kf) * imdb.train_batch_size)
+            policy.update(-np.log(terminal_reward))
+
+            message('First over index:', first_over_index)
+            message('Length of kf:', len(kf))
+            message('Training batch size:', imdb.train_batch_size)
+            message('Terminal reward:', terminal_reward)
         else:
             policy.update(1. - valid_err)
 
