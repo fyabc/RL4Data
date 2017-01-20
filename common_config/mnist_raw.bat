7798fea3e6b1f49a6a8f29c18f0b184a3fda25f4
fyabc <fyabc@mail.ustc.edu.cn>
2017-01-20 06:38:49 -0800

    More fix, test success in MNIST raw.

--- common_config/mnist_raw.bat
+++ common_config/mnist_raw.bat
@@ -1,2 +1,2 @@
 @rem The original valid_freq is 2500 (2500 * 20 = 50000 data, one valid point per epoch)
-python train.py G.dataset=mnist G.train_type=@raw@ valid_freq=250
+python train.py G.dataset=@mnist@ G.train_type=@raw@ valid_freq=250 epoch_per_episode=165 G.logging_file=@./log/mnist/log_M_raw.txt@
