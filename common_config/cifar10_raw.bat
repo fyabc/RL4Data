88c8d0b09bfca5c048e490e895405d917f80b5d4
fyabc <fyabc@mail.ustc.edu.cn>
2017-01-22 23:05:38 +0800

    Some small fix.

--- common_config/cifar10_raw.bat
+++ common_config/cifar10_raw.bat
@@ -2,3 +2,6 @@
 @rem data_dir=@./data/cifar10/cifar10.pkl.gz@
 @rem one_file=True (if False, it will read the data directory './data/cifar-10-batches-py')
 python train.py G.dataset=@cifar10@ G.train_type=@raw@ epoch_per_episode=60 G.logging_file=@./log/cifar10/log_C_raw.txt@
+
+@rem cifar10 uncorrupted raw:
+python train.py G.job_name=@cifar10-raw-NonC1@ epoch_per_episode=82 valid_freq=390
