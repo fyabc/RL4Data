c5b93cf5d793e822ff6cf897d6fd875b9ebbfda3
fyabc <fyabc@mail.ustc.edu.cn>
2017-02-05 21:47:49 +0800

    Add something in plot accuracy.

--- result_analysis/plot_accuracy.py
+++ result_analysis/plot_accuracy.py
@@ -33,7 +33,32 @@ def get_test_acc_list(filename, dataset='mnist', interval=Interval):
     return result
 
 
-def plot():
+def plot_mnist():
+    raw = get_test_acc_list('log-mnist-raw-NonC1.txt')
+    spl = get_test_acc_list('log-mnist-spl-NonC1.txt')
+
+    speed = get_test_acc_list('log-mnist-stochastic-lr-speed-NonC2.txt')
+    speed_best = get_test_acc_list('log-mnist-stochastic-lr-speed-NonC2Best.txt')
+    delta_acc = get_test_acc_list('log-mnist-stochastic-lr-delta_acc-NonC2.txt')
+    delta_acc_best = get_test_acc_list('log-mnist-stochastic-lr-delta_acc-NonC2Best.txt')
+
+    plt.plot(raw, label='raw')
+    plt.plot(spl, label='spl')
+
+    plt.plot(speed, label='speed last (325)')
+    plt.plot(speed_best, label='speed best (65)')
+    plt.plot(delta_acc, label='delta acc last (503)')
+    plt.plot(delta_acc_best, label='delta acc best (371)')
+
+    plt.ylim(ymin=0.88, ymax=0.98)
+    plt.xlim(xmax=1200 // Interval)
+
+    plt.legend(loc='lower right')
+
+    plt.show()
+
+
+def plot_c_mnist():
     raw = get_test_acc_list('log-mnist-raw-Flip1.txt')
     best80 = get_test_acc_list('log-mnist-raw-FlipBest80.txt')
     spl = get_test_acc_list('log-mnist-spl-ForceFlip1.txt')
@@ -46,21 +71,21 @@ def plot():
 
     speed = get_test_acc_list('log-mnist-stochastic-lr-Flip2.txt')
     speed_best = get_test_acc_list('log-mnist-stochastic-lr-speed-Flip2Best.txt')
-    delta_acc = get_test_acc_list('log-mnist-stochastic-lr-delta_acc-Flip2.txt')
+    delta_acc_best = get_test_acc_list('log-mnist-stochastic-lr-delta_acc-Flip2.txt')
 
     plt.plot(raw, label='raw')
-    plt.plot(best80, label='best 80%')
+    # plt.plot(best80, label='best 80%')
     plt.plot(spl, label='spl')
 
-    plt.plot(speed, label='speed')
-    plt.plot(speed_best, label='speed best')
-    plt.plot(delta_acc, label='delta acc')
+    plt.plot(speed, label='speed last (340)')
+    plt.plot(speed_best, label='speed best (286)')
+    plt.plot(delta_acc_best, label='delta acc best (213)')
 
     # plt.plot(m0r0b2, '--', label='m0r0b2')
     # plt.plot(m2r_2b2, '--', label='m2r-2b2')
-    plt.plot(m_2r2b2, '--', label='m-2r2b2')
-    plt.plot(m_3r3b2, '--', label='m-3r3b2')
-    plt.plot(m_4r4b2, '--', label='m-4r4b2')
+    # plt.plot(m_2r2b2, '--', label='m-2r2b2')
+    # plt.plot(m_3r3b2, '--', label='m-3r3b2')
+    # plt.plot(m_4r4b2, '--', label='m-4r4b2')
 
     plt.ylim(ymin=0.87, ymax=0.96)
     plt.xlim(xmax=1200 // Interval)
@@ -71,4 +96,6 @@ def plot():
 
 
 if __name__ == '__main__':
-    plot()
+    # plot_c_mnist()
+    plot_mnist()
+    pass
