2b053cad719c0dc113de34bf34635a38a8823a9a
fyabc <fyabc@mail.ustc.edu.cn>
2017-02-16 04:40:37 -0800

    More in train IMDB.

--- train_IMDB.py
+++ train_IMDB.py
@@ -13,14 +13,15 @@ from utils import *
 from utils_IMDB import pre_process_IMDB_data, pre_process_config
 from utils_IMDB import prepare_imdb_data as prepare_data
 from batch_updater import *
+from reward_checker import get_reward_checker, RewardChecker
 
 __author__ = 'fyabc'
 
 
 # TODO: Change code into updaters
 # raw: Done
-# spl: X
-# REINFORCE: X
+# spl: Done
+# REINFORCE: Done
 # A-C: X
 # Test: X
 
@@ -207,6 +208,10 @@ def train_raw_IMDB2():
             model.use_noise.set_value(floatX(1.))
             part_train_cost = updater.add_batch(train_index)
 
+            # Log training loss of each batch in test process
+            if part_train_cost is not None:
+                message("tL {}: {:.6f}".format(updater.epoch_train_batches, part_train_cost.tolist()))
+
             if updater.total_train_batches > 0 and \
                     updater.total_train_batches != last_validate_point and \
                     updater.total_train_batches % valid_freq == 0:
@@ -237,6 +242,8 @@ def train_raw_IMDB2():
         if early_stop:
             message('Early Stop!')
             break
+
+    episode_final_message(best_validate_acc, best_iteration, test_score, start_time)
 
 
 def train_SPL_IMDB():
@@ -392,6 +399,80 @@ def train_SPL_IMDB():
                           history_errs, best_parameters,
                           epoch, start_time, end_time,
                           save_to)
+
+
+def train_SPL_IMDB2():
+    # Loading data
+    # [NOTE] This must before the build of model, because the ParamConfig['ydim'] should be set.
+    x_train, y_train, x_validate, y_validate, x_test, y_test, \
+        train_size, valid_size, test_size = pre_process_IMDB_data()
+
+    model = IMDBModel()
+
+    # Loading configure settings
+    kf_valid, kf_test, \
+        valid_freq, save_freq, display_freq, \
+        save_to, patience = pre_process_config(model, train_size, valid_size, test_size)
+
+    updater = SPLUpdater(model, [x_train, y_train], ParamConfig['epoch_per_episode'], prepare_data=prepare_data)
+
+    # Train the network
+    # Some variables
+    # To prevent the double validate point
+    last_validate_point = -1
+
+    best_validate_acc = -np.inf
+    best_iteration = 0
+    test_score = 0.0
+    bad_counter = 0
+    early_stop = False
+    start_time = time.time()
+
+    for epoch in range(ParamConfig['epoch_per_episode']):
+        epoch_start_time = start_new_epoch(updater, epoch)
+
+        kf = get_minibatches_idx(train_size, model.train_batch_size, shuffle=True)
+
+        for _, train_index in kf:
+            model.use_noise.set_value(floatX(1.))
+            part_train_cost = updater.add_batch(train_index)
+
+            # Log training loss of each batch in test process
+            if part_train_cost is not None:
+                message("tL {}: {:.6f}".format(updater.epoch_train_batches, part_train_cost.tolist()))
+
+            if updater.total_train_batches > 0 and \
+                    updater.total_train_batches != last_validate_point and \
+                    updater.total_train_batches % valid_freq == 0:
+                last_validate_point = updater.total_train_batches
+
+                model.use_noise.set_value(floatX(0.))
+                validate_acc, test_acc = validate_point_message(
+                    model, x_train, y_train, x_validate, y_validate, x_test, y_test, updater,
+                    validate_size=valid_size,  # Use part validation set in baseline
+                    run_test=True,
+                )
+
+                if validate_acc > best_validate_acc:
+                    best_validate_acc = validate_acc
+                    best_iteration = updater.total_train_batches
+                    test_score = test_acc
+                    bad_counter = 0
+
+                if len(updater.history_accuracy) >= patience and \
+                        validate_acc <= max(updater.history_accuracy[:-patience]):
+                    bad_counter += 1
+                    if bad_counter > patience:
+                        early_stop = True
+                        break
+
+        message("Epoch {} of {} took {:.3f}s".format(
+            epoch, ParamConfig['epoch_per_episode'], time.time() - epoch_start_time))
+        if early_stop:
+            message('Early Stop!')
+            break
+
+    episode_final_message(best_validate_acc, best_iteration, test_score, start_time)
 
 
 def train_policy_IMDB():
@@ -558,6 +639,114 @@ def train_policy_IMDB():
             message('Terminal reward:', terminal_reward)
         else:
             policy.update(1. - valid_err)
+
+        if PolicyConfig['policy_save_freq'] > 0 and episode % PolicyConfig['policy_save_freq'] == 0:
+            policy.save_policy(PolicyConfig['policy_save_file'], episode)
+
+
+def train_policy_IMDB2():
+    # Loading data
+    # [NOTE] This must before the build of model, because the ParamConfig['ydim'] should be set.
+    x_train, y_train, x_validate, y_validate, x_test, y_test, \
+        train_size, valid_size, test_size = pre_process_IMDB_data()
+
+    model = IMDBModel()
+
+    # Build policy
+    input_size = model.get_policy_input_size()
+    message('Input size of policy network:', input_size)
+    policy = PolicyNetworkBase.get_by_name(PolicyConfig['policy_model_type'])(input_size=input_size)
+    # policy = LRPolicyNetwork(input_size=input_size)
+
+    policy.check_load()
+
+    # Loading configure settings
+    kf_valid, kf_test, \
+        valid_freq, save_freq, display_freq, \
+        save_to, patience = pre_process_config(model, train_size, valid_size, test_size)
+
+    reward_checker_type = RewardChecker.get_by_name(PolicyConfig['reward_checker'])
+
+    start_episode = 1 + PolicyConfig['start_episode']
+    for episode in range(start_episode, start_episode + PolicyConfig['num_episodes']):
+        start_new_episode(model, policy, episode)
+
+        # Train the network
+        # Some variables
+
+        # To prevent the double validate point
+        last_validate_point = -1
+
+        # get small training data
+        # x_train_small, y_train_small = get_part_data(x_train, y_train, ParamConfig['train_small_size'])
+        # [WARNING] Do NOT shuffle here!!!
+        x_train_small, y_train_small = x_train, y_train
+
+        train_small_size = len(x_train_small)
+        message('Training small size:', train_small_size)
+
+        # Speed reward
+        reward_checker = get_reward_checker(
+            reward_checker_type,
+            ParamConfig['epoch_per_episode'] * train_small_size
+        )
+
+        updater = TrainPolicyUpdater(model, [x_train_small, y_train_small], policy, prepare_data=prepare_data)
+
+        best_validate_acc = -np.inf
+        best_iteration = 0
+        test_score = 0.0
+        bad_counter = 0
+        early_stop = False
+        start_time = time.time()
+
+        for epoch in range(ParamConfig['epoch_per_episode']):
+            epoch_start_time = start_new_epoch(updater, epoch)
+
+            kf = get_minibatches_idx(train_size, model.train_batch_size, shuffle=True)
+
+            for _, train_index in kf:
+                model.use_noise.set_value(floatX(1.))
+                part_train_cost = updater.add_batch(train_index)
+
+                # Log training loss of each batch in test process
+                if part_train_cost is not None:
+                    message("tL {}: {:.6f}".format(updater.epoch_train_batches, part_train_cost.tolist()))
+
+                if updater.total_train_batches > 0 and \
+                        updater.total_train_batches != last_validate_point and \
+                        updater.total_train_batches % valid_freq == 0:
+                    last_validate_point = updater.total_train_batches
+
+                    model.use_noise.set_value(floatX(0.))
+                    validate_acc, test_acc = validate_point_message(
+                        model, x_train, y_train, x_validate, y_validate, x_test, y_test, updater,
+                        validate_size=valid_size,  # Use part validation set in baseline
+                        run_test=False,
+                    )
+
+                    if validate_acc > best_validate_acc:
+                        best_validate_acc = validate_acc
+                        best_iteration = updater.total_train_batches
+                        test_score = test_acc
+                        bad_counter = 0
+
+                    if len(updater.history_accuracy) >= patience and \
+                            validate_acc <= max(updater.history_accuracy[:-patience]):
+                        bad_counter += 1
+                        if bad_counter > patience:
+                            early_stop = True
+                            break
+
+            message("Epoch {} of {} took {:.3f}s".format(
+                epoch, ParamConfig['epoch_per_episode'], time.time() - epoch_start_time))
+            if early_stop:
+                message('Early Stop!')
+                break
+
+        episode_final_message(best_validate_acc, best_iteration, test_score, start_time)
+
+        policy.update(reward_checker)
 
         if PolicyConfig['policy_save_freq'] > 0 and episode % PolicyConfig['policy_save_freq'] == 0:
             policy.save_policy(PolicyConfig['policy_save_file'], episode)
@@ -925,12 +1114,12 @@ def main2(args=None):
 def main():
     dataset_main({
         'raw': train_raw_IMDB2,
-        'self_paced': train_SPL_IMDB,
-        'spl': train_SPL_IMDB,
+        'self_paced': train_SPL_IMDB2,
+        'spl': train_SPL_IMDB2,
 
-        'policy': train_policy_IMDB,
-        'reinforce': train_policy_IMDB,
-        'speed': train_policy_IMDB,
+        'policy': train_policy_IMDB2,
+        'reinforce': train_policy_IMDB2,
+        'speed': train_policy_IMDB2,
 
         'actor_critic': train_actor_critic_IMDB,
         'ac': train_actor_critic_IMDB,
